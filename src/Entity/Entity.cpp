234a52d0957827634973ef8249add566c9009ced
Gillifish <dkgilliland@gillinet.org>
2023-07-05 21:37:57 -0400

    Formatting

--- src/Entity/Entity.cpp
+++ src/Entity/Entity.cpp
@@ -1,9 +1,21 @@
 #include "Entity.h"
 
-void Entity::destroy() { m_active = false; }
+void Entity::destroy()
+{
+    m_active = false;
+}
 
-const std::string &Entity::tag() const { return m_tag; }
+const std::string &Entity::tag() const
+{
+    return m_tag;
+}
 
-const size_t Entity::id() const { return m_id; }
+const size_t Entity::id() const
+{
+    return m_id;
+}
 
-bool Entity::isActive() const { return m_active; }
+bool Entity::isActive() const
+{
+    return m_active;
+}
