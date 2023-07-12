dd5edfa4d8e0561c336f52f199750092a2f1fbe6
Gillifish <dkgilliland@gillinet.org>
2023-07-12 03:20:46 -0400

    Added boilerplate code to TestGame.cpp

--- src/TestGame.h
+++ src/TestGame.h
@@ -1,11 +1,12 @@
 #pragma once
 
 #include "Engine/Entity/EntityManager.h"
+#include "Engine/Scene/Scene.h"
 #include <SFML/Graphics.hpp>
 
 
 // Class used for testing various functionality of the game engine
-class TestGame
+class TestGame : Scene
 {
 
 };
